e826bb9a6956d4c003e89f45fa4a2f70f4410e41
HW3 User <practice@guest.guest>
2019-09-19 18:53:45 +0000

    changes

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,8 @@ use Mix.Config
 
 # Configures the endpoint
 config :practice, PracticeWeb.Endpoint,
-  url: [host: "localhost"],
+  #url: [host: "localhost"],
+  url: [host: "hw03.mgrathena.info"],
   secret_key_base: "cCJYQLnt/HqBGqyowai4HvJ6Y0lF1jle6FiAQP6fZehGCDNX8lPwQMD/+S/tLkOu",
   render_errors: [view: PracticeWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Practice.PubSub,
